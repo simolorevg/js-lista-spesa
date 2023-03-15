2cd4b331f2224de239cb9b1bb0b210d9a76c7c82
simolorevg <simoneviaggi@libero.it>
2023-03-15 17:21:59 +0100

    obviously I did not understand the delivery of the exercise. I redo the first part

--- js/script.js
+++ js/script.js
@@ -1,15 +1,8 @@
-//prendo l'elemento inserito dall'utente
-const userProductInput = document.getElementById('userInput');//prendo l'input html
-const submitBtn = document.getElementById('submit-btn');//prendo il bottone
-const userProduct = userProductInput.value;//estraggo il valore dall'input
-const productArray=[];//creo array di prodotti
+
 const productList = document.getElementById('productList');//prendo la lista non ordinata
 let productIndex = 0;
-let i = 0;
 submitBtn.addEventListener('click', function(){
-    while(productIndex < productArray.lenght - 1){
-        
-    }
+    productList.innerHTML+=`<li>${userProduct}</li>`;
+    //finche l'elemento non è presente, lo aggiungo nella lista
+    //altrimenti non lo metto
 });
-//finche l'elemento non è presente, lo aggiungo nella lista
-//altrimenti non lo metto
