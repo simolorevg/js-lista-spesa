de17b81676f226e8e066501c87122fd472bf860a
simolorevg <simoneviaggi@libero.it>
2023-03-15 17:52:14 +0100

    Add some style to the page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-const listArray = ['pane','salame','uova','fruttini'];
+const listArray = ['pane','salame','uova','fruttini','Coca Cola'];//array lista spesa
 const productList = document.getElementById('product-list'); //prendo la lista non ordinata
-let i = 0;
+let i = 0;//contatore
 console.log(listArray.length);
-while(i < listArray.length){
-    productList.innerHTML+=`<li>${listArray[i]}</li>`;
-    console.log(listArray[i]);
-    i++;
+while(i < listArray.length){//finchè i è minore della lunghezza dell'array
+    productList.innerHTML+=`<li>${listArray[i]}</li>`;//stampa sulla lista il prodotto
+    i++;//incrementa il contatore
 }
