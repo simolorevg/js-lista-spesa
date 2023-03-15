b4ef64dcbe3e31c0465df588b20ef0a81b3fd433
simolorevg <simoneviaggi@libero.it>
2023-03-15 15:48:56 +0100

    Working on the script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 //prendo l'elemento inserito dall'utente
+const userProductInput = document.getElementById('userInput');//prendo l'input html
+const submitBtn = document.getElementById('submit-btn');//prendo il bottone
+const userProduct = userProductInput.value;//estraggo il valore dall'input
+submitBtn.addEventListener('click', function(){
+    console.log(userProduct);
+    userProductInput.value="";
+});
 //finche l'elemento non è presente, lo aggiungo nella lista
 //altrimenti non lo metto
